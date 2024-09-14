c3d63047bb9746a6e5f34f4481f3e8fe9411829b
Sanal Kumar <sanaltsk@gmail.com>
2024-09-13 20:58:02 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,51 @@
+import openai
+import os
+import chainlit as cl
+import base64
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+endpoint_url = "https://api.openai.com/v1"
+client = openai.AsyncClient(api_key=api_key, base_url=endpoint_url)
+
+# https://platform.openai.com/docs/models/gpt-4o
+model_kwargs = {
+    "model": "chatgpt-4o-latest",
+    "temperature": 1.2,
+    "max_tokens": 500
+}
+
+# model_kwargs = {
+#     "model": "mistralai/Mistral-7B-Instruct-v0.3",
+#     "temperature": 0.3,
+#     "max_tokens": 500
+# }
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    #Maintain an array of messages in the user session
+    message_history = cl.user_session.get("message_history", [])
+    message_history.append({'role': 'user', 'content': message.content})
+
+    response_msg = cl.Message(content="")
+    await response_msg.send()
+
+    # Pass in the full message history for each request
+    stream = await client.chat.completions.create(
+        messages=message_history, stream=True, **model_kwargs
+    )
+
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await response_msg.stream_token(token)
+    await response_msg.update()
+
+    # Record the AI's response in the history
+    message_history.append(
+        {'role':'assistant','content':response_msg.content}
+    )
+    cl.user_session.set("message_history",message_history)
+
+
+
+
