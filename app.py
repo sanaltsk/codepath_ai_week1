82f5a5b585d75e1a08820a3ae46bb6d28ed69564
Sanal Kumar <sanaltsk@gmail.com>
2024-09-13 20:58:32 -0700

    adding system prompt

--- app.py
+++ app.py
@@ -1,30 +1,62 @@
 import openai
 import os
 import chainlit as cl
-import base64
+from dotenv import load_dotenv
+from prompts import SYSTEM_PROMPT
+from langsmith import traceable
+from langsmith.wrappers import wrap_openai
 
-api_key = os.getenv("OPENAI_API_KEY")
+# Load environment variables
+load_dotenv()
+ENABLE_SYSTEM_PROMPT = True
 
-endpoint_url = "https://api.openai.com/v1"
-client = openai.AsyncClient(api_key=api_key, base_url=endpoint_url)
+configurations = {
+    "mistral_7B_instruct": {
+        "endpoint_url": os.getenv("MISTRAL_7B_INSTRUCT_ENDPOINT"),
+        "api_key": os.getenv("RUNPOD_API_KEY"),
+        "model": "mistralai/Mistral-7B-Instruct-v0.2"
+    },
+    "mistral_7B": {
+        "endpoint_url": os.getenv("MISTRAL_7B_ENDPOINT"),
+        "api_key": os.getenv("RUNPOD_API_KEY"),
+        "model": "mistralai/Mistral-7B-v0.1"
+    },
+    "openai_gpt-4": {
+        "endpoint_url": os.getenv("OPENAI_ENDPOINT"),
+        "api_key": os.getenv("OPENAI_API_KEY"),
+        "model": "gpt-4"
+    }
+}
 
-# https://platform.openai.com/docs/models/gpt-4o
-model_kwargs = {
-    "model": "chatgpt-4o-latest",
-    "temperature": 1.2,
+# Choose configuration
+config_key = "openai_gpt-4"
+# config_key = "mistral_7B_instruct"
+#config_key = "mistral_7B"
+
+# Get selected configuration
+config = configurations[config_key]
+
+# Initialize the OpenAI async client
+client = wrap_openai(openai.AsyncClient(api_key=config["api_key"], base_url=config["endpoint_url"]))
+
+gen_kwargs = {
+    "model": config["model"],
+    "temperature": 0.3,
     "max_tokens": 500
 }
 
-# model_kwargs = {
-#     "model": "mistralai/Mistral-7B-Instruct-v0.3",
-#     "temperature": 0.3,
-#     "max_tokens": 500
-# }
 
 @cl.on_message
+@traceable
 async def on_message(message: cl.Message):
     #Maintain an array of messages in the user session
     message_history = cl.user_session.get("message_history", [])
+
+    if ENABLE_SYSTEM_PROMPT and (not message_history or message_history[0].get("role") != "system"):
+        system_prompt_content = SYSTEM_PROMPT
+        message_history.insert(0, {"role": "system", "content": system_prompt_content})
+
+
     message_history.append({'role': 'user', 'content': message.content})
 
     response_msg = cl.Message(content="")
@@ -32,7 +64,7 @@ async def on_message(message: cl.Message):
 
     # Pass in the full message history for each request
     stream = await client.chat.completions.create(
-        messages=message_history, stream=True, **model_kwargs
+        messages=message_history, stream=True, **gen_kwargs
     )
 
     async for part in stream:
---
+++ prompts.py
@@ -0,0 +1,21 @@
+SYSTEM_PROMPT = """
+You are an expert in technology and have an expertise in tablets. You are aware of the most latest
+tablets in the market. You have a unique skill of identifying the best suited tablets and have been 
+able to recommend it to people around you based on the usage, budget and features required by them.
+
+To suggest a good tablet.
+1. Identify the age of the user, a kid using a tablet is mostly for entertainment or education purposes
+a teenager would use the tablet for learning, social media and messaging. Adult might use the tablet 
+for browsing, learning, videos etc.
+
+2. Identify the budget of the user, this is a critical factor based on which many people decide on 
+which device to buy
+
+3. Feature set, another factor is the feature set included in the tablet.
+
+Based on the above three factors help users decided on the best laptop suiting their needs, ask followup
+questions to narrow down the above three factors and suggest the best tablet.
+
+If asked anything other than tablets, respond with saying that your expertise is only in the domain of tablets.
+
+"""
